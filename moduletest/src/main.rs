894b1ffe978b5c0afd4d4286089b3865882485c7
ShintaniDavid <shintanidavid@icloud.com>
2022-10-06 22:48:02 +0900

    main.rs

--- moduletest/src/main.rs
+++ moduletest/src/main.rs
@@ -1,12 +1,37 @@
-struct SeaCreature{
-    //Stringは構造体らしい。
+struct SeaCreature {
     animal_type: String,
     name: String,
     arms: i32,
-    legs: f32,
+    legs: i32,
     weapon: String,
-    //フィールドは、データ構造とキーワードを紐づける
-    //値はプリミティブ型もしくはデータ構造を指定可能。
-    //メモリ上で隣り合うデータの配置をコンパイラに伝えるが、メモリとコンパイルに関して
-    //知識が浅いので用復習
+}
+
+fn main() {
+    // SeaCreatureのデータはスタックに入ります。
+    let ferris = SeaCreature {
+        // String構造体もスタックに入りますが、
+        // ヒープに入るデータの参照アドレスが一つ入ります。
+        animal_type: String::from("crab"),
+        name: String::from("Ferris"),
+        arms: 2,
+        legs: 4,
+        weapon: String::from("claw"),
+    };
+
+    let sarah = SeaCreature {
+        animal_type: String::from("octopus"),
+        name: String::from("Sarah"),
+        arms: 8,
+        legs: 0,
+        weapon: String::from("none"),
+    };
+    
+    println!(
+        "{} is a {}. They have {} arms, {} legs, and a {} weapon",
+        ferris.name, ferris.animal_type, ferris.arms, ferris.legs, ferris.weapon
+    );
+    println!(
+        "{} is a {}. They have {} arms, and {} legs. They have no weapon..",
+        sarah.name, sarah.animal_type, sarah.arms, sarah.legs
+    );
 }
