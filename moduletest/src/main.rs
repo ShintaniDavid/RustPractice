7d2de9fef3a5778201b48670ce8dcc8f79876866
ShintaniDavid <shintanidavid@icloud.com>
2022-10-10 23:02:29 +0900

    main.rs

--- moduletest/src/main.rs
+++ moduletest/src/main.rs
@@ -1,8 +1,6 @@
-struct SeaCreature {
-    // String は構造体である。
-    animal_type: String,
-    name: String,
-    arms: i32,
-    legs: i32,
-    weapon: String,
+fn main() {
+    // スタティックメソッドでStringインスタンスを作成する。
+    let s = String::from("Hello world!");
+    // インスタンスを使ってメソッド呼び出す。
+    println!("{} is {} characters long.", s, s.len());
 }
