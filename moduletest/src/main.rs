57ced947f9d824fe3dae5c264f6f76b6557ad798
ShintaniDavid <shintanidavid@icloud.com>
2022-10-14 17:59:09 +0900

    main.rs

--- moduletest/src/main.rs
+++ moduletest/src/main.rs
@@ -1,7 +1,5 @@
-struct Location(i32, i32);
+struct Marker;
 
 fn main() {
-    // これもスタックに入れられる構造体です。
-    let loc = Location(42, 32);
-    println!("{}, {}", loc.0, loc.1);
+    let _m = Marker;
 }
