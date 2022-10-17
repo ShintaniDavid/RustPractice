33cd05f55b5bc64a9043571f37a86d289ee2f7de
ShintaniDavid <shintanidavid@icloud.com>
2022-10-18 06:45:27 +0900

    main.rs

--- moduletest/src/main.rs
+++ moduletest/src/main.rs
@@ -1,10 +1,12 @@
 #![allow(dead_code)] // この行でコンパイラのwaringsメッセージを止めます。
 
-enum Species {
-    Crab,
-    Octopus,
-    Fish,
-    Clam
+enum Species { Crab, Octopus, Fish, Clam }
+enum PoisonType { Acidic, Painful, Lethal }
+enum Size { Big, Small }
+enum Weapon {
+    Claw(i32, Size),
+    Poison(PoisonType),
+    None
 }
 
 struct SeaCreature {
@@ -12,22 +14,34 @@ struct SeaCreature {
     name: String,
     arms: i32,
     legs: i32,
-    weapon: String,
+    weapon: Weapon,
 }
 
 fn main() {
+    // SeaCreatureのデータはスタックに入ります。
     let ferris = SeaCreature {
+        // String構造体もスタックに入りますが、
+        // ヒープに入るデータの参照アドレスが一つ入ります。
         species: Species::Crab,
         name: String::from("Ferris"),
         arms: 2,
         legs: 4,
-        weapon: String::from("claw"),
+        weapon: Weapon::Claw(2, Size::Small),
     };
 
     match ferris.species {
-        Species::Crab => println!("{} is a crab",ferris.name),
-        Species::Octopus => println!("{} is a octopus",ferris.name),
-        Species::Fish => println!("{} is a fish",ferris.name),
-        Species::Clam => println!("{} is a clam",ferris.name),
+        Species::Crab => {
+            match ferris.weapon {
+                Weapon::Claw(num_claws,size) => {
+                    let size_description = match size {
+                        Size::Big => "big",
+                        Size::Small => "small"
+                    };
+                    println!("ferris is a crab with {} {} claws", num_claws, size_description)
+                },
+                _ => println!("ferris is a crab with some other weapon")
+            }
+        },
+        _ => println!("ferris is some other animal"),
     }
 }
